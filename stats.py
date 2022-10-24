946ee2ff3404781b0dd42dfb2470c18a9a3a7b76
Pedro Stanzani <pedrohstanzani@gmail.com>
2022-10-24 15:52:30 -0300

    Add Binomial distribution wrapper

---
+++ stats.py
@@ -0,0 +1,35 @@
+from scipy import stats
+
+"""
+Usage:
+A salesman makes 10 calls. Each call is an independent event.
+The probability that he will close an individual sale is 0.3.
+
+>>> b = BinomialWrapper(10, 0.3)
+
+# What is the probability of closing 3 or more sales?
+>>> b.greater_than_or_equal_to(3)
+
+# What is the probability of closing exactly 4 sales?
+>>> b.equal_to(4)
+"""
+
+class BinomialWrapper:
+    def __init__(self, n, p):
+        self.n = n  # number of independent events
+        self.p = p  # probability of success
+    
+    def equal_to(self, y):
+        return stats.binom.pmf(y, self.n, self.p)
+    
+    def less_than_or_equal_to(self, y):
+        return stats.binom.cdf(y, self.n, self.p)
+    
+    def greater_than(self, y):
+        return stats.binom.sf(y, self.n, self.p)
+    
+    def less_than(self, y):
+        return stats.binom.cdf(y, self.n, self.p) - stats.binom.pmf(y, self.n, self.p)
+    
+    def greater_than_or_equal_to(self, y):
+        return stats.binom.sf(y, self.n, self.p) + stats.binom.pmf(y, self.n, self.p)
